7a38de94de100528d6e64587b976c6b635b4e028
CaptainSlimm <112794982+CaptainSlimm@users.noreply.github.com>
2022-09-05 12:04:48 -0400

    Add files via upload

---
+++ if_else_study.py
@@ -0,0 +1,15 @@
+password = 'kamehameha'
+print('enter password')
+input(password)
+if password == input(''):
+    print('Access Granted.')
+if password == input(''):
+    print('Dragon Ball Z')
+    print('Press Start to Play')
+    print('Choose')
+    print('1 Player')
+    print('2 Player')
+    print('Story Mode')
+    print('Battle Mode')
+else:
+    print('Access Denied')
