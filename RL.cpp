db074ce8854f118e96c731b4b05ff57a4d1112b6
CaptainSlimm <112794982+CaptainSlimm@users.noreply.github.com>
2023-07-26 16:50:20 -0400

    Add files via upload
    Random Loop practice in C++

---
+++ RL.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+int main()
+{
+	random.txt;
+}
+ifstream myinputfile;
+ofstream myoutputfile;
+
+myinputfile.open("random.txt");
+
+while (myinputfile >> grade) //read from the 
+{
+	cout << grade << endl;
+
+}
+//close the file
